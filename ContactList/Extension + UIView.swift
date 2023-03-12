14dd3d1f3ca45181a66806979276bf35407b33af
JuliaRomanenko <rja-97@yandex.ru>
2023-03-12 20:37:02 +0300

    Добавила расширение для UIView

---
+++ ContactList/Extension + UIView.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension UIView {
+    func configureSeparator() {
+        backgroundColor = UIColor(red: 84/255, green: 84/255, blue: 88/255, alpha: 0.65)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
