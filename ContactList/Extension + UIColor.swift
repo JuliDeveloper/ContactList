125b955a539934066c1794d25f8929d1e4ee6b39
JuliaRomanenko <rja-97@yandex.ru>
2023-03-12 17:15:13 +0300

    Добавила кастомные цвета в проект

---
+++ ContactList/Extension + UIColor.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIColor {
+    static var customWhite: UIColor {UIColor(named: "customWhite") ?? UIColor()}
+    static var customBlack: UIColor {UIColor(named: "customBlack") ?? UIColor()}
+    static var customDarkGray: UIColor {UIColor(named: "customDarkGray") ?? UIColor()}
+    static var customLightGray: UIColor {UIColor(named: "customLightGray") ?? UIColor()}
+    static var customBlue: UIColor {UIColor(named: "customBlue") ?? UIColor()}
+    static var customRed: UIColor {UIColor(named: "customRed") ?? UIColor()}
+}
