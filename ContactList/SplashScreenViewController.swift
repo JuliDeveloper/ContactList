4abcd5be5236bd995dccac6086efbcd097b918c5
JuliaRomanenko <rja-97@yandex.ru>
2023-03-12 20:38:19 +0300

    Внесла дополнительные ui-элементы и логику в SplashVC

--- ContactList/SplashScreenViewController.swift
+++ ContactList/SplashScreenViewController.swift
@@ -9,22 +9,90 @@ final class SplashScreenViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var settingsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Хочу увидеть свои контакты", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.backgroundColor = .customBlue
+        button.tintColor = .customWhite
+        button.layer.cornerRadius = 24
+        button.isHidden = true
+        button.addTarget(
+            self,
+            action: #selector(openSettings),
+            for: .touchUpInside
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let customAlert = CustomAlert()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
         setupConstraints()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            guard let self = self else { return }
+            self.customAlert.showAlert(
+                title: "Разрешить приложению «ContactsList» доступ к контактам?",
+                view: self.view) {
+                    print("Доступ разрешен")
+                    let listVC = ContactListTableView()
+                    self.present(listVC, animated: true)
+                } cancelHandler: {
+                    print("Доступ запрещен")
+                    self.settingsButton.isHidden = false
+                }
+        }
     }
     
     private func configureView() {
         view.backgroundColor = .customBlack
         
         view.addSubview(logoImageView)
+        view.addSubview(settingsButton)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            logoImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            logoImageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+            logoImageView.centerXAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.centerXAnchor
+            ),
+            logoImageView.centerYAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.centerYAnchor
+            )
         ])
+        
+        NSLayoutConstraint.activate([
+            settingsButton.heightAnchor.constraint(
+                equalToConstant: 64
+            ),
+            settingsButton.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
+            settingsButton.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            settingsButton.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: -58
+            )
+        ])
+    }
+    
+    @objc private func openSettings() {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                print("Settings opened: \(success)")
+            })
+        }
     }
 }
